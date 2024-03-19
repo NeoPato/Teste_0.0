6e1064bda11d14f42562b167181db8875d595b57
NeoPato <gabrielhmsena20@gmail.com>
2024-03-19 16:51:19 -0300

    CoisaLinda

---
+++ vite-project/src/home.jsx
@@ -0,0 +1,11 @@
+import Graficos from "./graficos"
+
+export const Home = () =>{
+    return(
+        <>
+            <Graficos text="Grafico bonito" color="#146b12"/>
+            <Graficos text="Grafico Linda" color="#ad4e0a"/>
+            <Graficos text="Grafico Gatinha" color="#743deb"/>
+        </>
+    )
+}
